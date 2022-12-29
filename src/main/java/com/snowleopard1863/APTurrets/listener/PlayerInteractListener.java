c11908c6c329810953a69e142848e23953443941
TylerS1066 <TylerS1066@users.noreply.github.com>
2022-12-29 15:48:43 -0600

    Remove debug messages

--- src/main/java/com/snowleopard1863/APTurrets/listener/PlayerInteractListener.java
+++ src/main/java/com/snowleopard1863/APTurrets/listener/PlayerInteractListener.java
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.NotNull;
 public class PlayerInteractListener implements Listener {
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
     public void onClick(PlayerInteractEvent event) {
-        TurretsMain.getInstance().getLogger().info("onClick");
         switch (event.getAction()) {
             case RIGHT_CLICK_AIR:
                 rightClick(event);
@@ -38,21 +37,17 @@ public class PlayerInteractListener implements Listener {
     }
 
     private void leftClickBlock(@NotNull PlayerInteractEvent event) {
-        TurretsMain.getInstance().getLogger().info("leftClickBlock");
         if (event.getPlayer().getInventory().getItemInMainHand().getType() != Material.STONE_BUTTON)
             return;
 
-        TurretsMain.getInstance().getLogger().info("is stone button");
         Material type = event.getClickedBlock().getType();
         if(!Tag.SIGNS.isTagged(type))
             return;
 
-        TurretsMain.getInstance().getLogger().info("is sign");
         Sign sign = (Sign) event.getClickedBlock().getState();
         if (!sign.getLine(0).equalsIgnoreCase("Mounted") || !sign.getLine(1).equalsIgnoreCase("Gun"))
             return;
     
-        TurretsMain.getInstance().getLogger().info("is mounted gun");
         // If a player left clicks the mounted gun with a stone button in hand,
         //  show them the statistics for the plugin (Damage, Knockback, Velocity, etc.)
         event.getPlayer().sendMessage("\n"
@@ -66,21 +61,17 @@ public class PlayerInteractListener implements Listener {
     }
 
     private void rightClickBlock(@NotNull PlayerInteractEvent event) {
-        TurretsMain.getInstance().getLogger().info("rightClickBlock");
         Material type = event.getClickedBlock().getType();
         if(!Tag.SIGNS.isTagged(type))
             return;
 
-        TurretsMain.getInstance().getLogger().info("is sign");
         Sign sign = (Sign) event.getClickedBlock().getState();
         if (!sign.getLine(0).equalsIgnoreCase("Mounted") || !sign.getLine(1).equalsIgnoreCase("Gun"))
             return;
 
-        TurretsMain.getInstance().getLogger().info("is mounted gun");
         if (event.getClickedBlock().getRelative(BlockFace.DOWN).getType() == Material.SLIME_BLOCK)
             return;
 
-        TurretsMain.getInstance().getLogger().info("is not slime");
         Location signPos = event.getClickedBlock().getLocation();
         signPos.setPitch(event.getPlayer().getLocation().getPitch());
         signPos.setDirection(event.getPlayer().getVelocity());
@@ -88,12 +79,10 @@ public class PlayerInteractListener implements Listener {
     }
 
     private void rightClick(PlayerInteractEvent event) {
-        TurretsMain.getInstance().getLogger().info("rightClick");
         Player player = event.getPlayer();
         if (!TurretsMain.getInstance().getTurretManager().isOnTurret(player) || !player.hasPermission("ap-turrets.use"))
             return;
 
-        TurretsMain.getInstance().getLogger().info("is on turret & has permission");
         if (player.getInventory().getItemInMainHand().getType() == Material.MILK_BUCKET
                 || player.getInventory().getItemInOffHand().getType() == Material.MILK_BUCKET) {
             // If the player tries to use milk to clear the effects, cancel the event to keep that from happening
@@ -101,16 +90,13 @@ public class PlayerInteractListener implements Listener {
             return;
         }
 
-        TurretsMain.getInstance().getLogger().info("is not milk");
         if (player.getInventory().getItemInMainHand().getType() != Material.STONE_BUTTON
                 && player.getInventory().getItemInOffHand().getType() != Material.STONE_BUTTON)
             return;
 
-        TurretsMain.getInstance().getLogger().info("is stone button");
         if (TurretsMain.getInstance().getTurretManager().isReloading(player))
             return;
 
-        TurretsMain.getInstance().getLogger().info("is not reloading");
         // Fires the turret and keeps them from interacting with something else and placing the button accidentally
         TurretsMain.getInstance().getTurretManager().fire(player);
         event.setCancelled(true);
